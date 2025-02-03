556eb43e84fe8234060539e89d189312fabe85ed
harveymulroney1 <harveymulroney@gmail.com>
2025-02-03 12:07:38 +0000

    Recipe Photo Upload

--- backend/receiptOCR.js
+++ backend/receiptOCR.js
@@ -2,7 +2,7 @@ import { createWorker } from 'tesseract.js';
 
 (async () => {
   const worker = await createWorker('eng');
-  const ret = await worker.recognize('C:/Users/harvey/Desktop/201Project/PlateFul/assets/images/users/receiptTest.jpeg');
+  const ret = await worker.recognize('C:/Users/harvey/Desktop/201Project/PlateFul/assets/receipts/test2.jpg');
   console.log(ret.data.text);
   await worker.terminate();
 })();
