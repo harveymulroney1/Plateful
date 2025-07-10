93ba364cea9d42b1531dcb8684ebd28dd91ea856
harveymulroney1 <harveymulroney@gmail.com>
2025-07-10 22:54:47 +0100

    Basic Shopping List and + or -

---
+++ app/shoppinglist.tsx
@@ -0,0 +1,434 @@
+import { Ionicons } from '@expo/vector-icons';
+import { router } from 'expo-router';
+import { useState } from 'react';
+import { Button,Alert, FlatList, StyleSheet, Text, TextInput, TouchableOpacity, View, ScrollView, ImageBackground, Dimensions } from 'react-native';
+import { Snackbar } from 'react-native-paper';
+import { useSnackbar } from "./snackbar";
+import Recipe from './recipe';
+export default function shoppingList()
+{
+   type Recipe = {
+    recipeName: string;
+    img: string;
+    ingredients:string[];
+    keywords: string[];
+  }
+  
+  type ItemProps = { title: string; img?:string; keywords?:string[]; onPress: () => void };
+  const [index,setIndex] = useState(0); // Placeholder for current index, can be updated based on user interaction
+  const Item = ({ title, img, keywords, onPress }: ItemProps) => (
+      <TouchableOpacity onPress={onPress} style={styles.item}>
+          <ImageBackground
+              source={img ? { uri: img } : require('../assets/images/food-image.png')}
+              style={styles.foodImage}
+              imageStyle={{ borderRadius: 12 }}
+          >
+              <View style={styles.textContainer}>
+                  <Text style={styles.recipeTitle}>{title}</Text>
+                  <View style={styles.labelsContainer}>
+                      {Array.isArray(keywords) && keywords.slice(0, 10).map((kw, index) => (
+                          <TouchableOpacity key={index} style={styles.label}>
+                              <Text style={styles.labelText}>{kw}</Text>
+                          </TouchableOpacity>
+                      ))}
+                  </View>
+              </View>
+          </ImageBackground>
+      </TouchableOpacity>
+  );
+    const [inputText, setInputText] = useState('');
+    const { showError } = useSnackbar();
+    const [shoppingList,setShoppingList] = useState<string[]>([]); 
+    const [recipesToAdd, setRecipesToAdd] = useState<Recipe[]>([]); // list of recipes to add to the shopping list - User Added
+    const [recipes, setRecipes] = useState<Recipe[]>([{ recipeName:"Chicken Curry",img:"" ,keywords: ["chicken", "curry", "coconut milk"],ingredients:["Chicken 500g","Coconut Milk 1 can"," 2 Peppers"]},{recipeName:"Chicken Beef",img:"" ,keywords: ["chicken", "curry", "coconut milk"],ingredients:["Beef 1kg","1 can Coconut Milk"," 1 Pepper"]},{recipeName:"Pork Curry",img:"" ,keywords: ["Pork", "curry", "coconut milk"],ingredients:["Pork 500g","Coconut Milk 1 can"," 2 Peppers"]}]);
+    const updateIngredient = (text:string, index: number) => {
+    const updated = [...shoppingList];
+    updated[index] = text;
+    setShoppingList(updated);
+    
+  };
+  const [showOverlay, setShowOverlay] = useState(false);
+    
+    function removeIngredient(index:number)
+    {
+    const updated = [...shoppingList];
+    const x = updated.splice(index,1);
+    setShoppingList(updated);
+    console.log("Removed elem: ",x);
+    }
+    const populateShoppingList = () => {
+      // loop through recipes selected and add the ingredients to the list
+      // Need to connect the ingredients together - multiple recipes using chicken
+      
+      // FOR NOW JUST BASIC POPULATE
+      
+      recipesToAdd.forEach(element => {
+        //setShoppingList([shoppingList,element.ingredients])
+        shoppingList.push(...element.ingredients)
+        
+      });
+      console.log(shoppingList);
+      setRecipesToAdd([]); // clear recipes to add
+    }
+    const handleAddItem = () => {
+        if (inputText.trim()) {
+        setShoppingList([...shoppingList, inputText.trim()]);
+        setInputText("");
+        }
+        else {
+            showError("Please enter an ingredient.");
+        }
+    }
+    function nextItem() {
+      console.log("index:", index, "recipes.length:", recipes.length);
+        if (index < recipes.length -1) {
+            setIndex(index + 1);
+            console.log(index);
+        } else {
+            // remove overlay and show shopping list
+            populateShoppingList();
+            toggleOverlay();
+            
+        }
+    }
+    const toggleOverlay = () => {
+        setShowOverlay(!showOverlay);
+    }
+    function addRecipe () {
+      console.log("added recipe");
+      setRecipesToAdd([...recipesToAdd,recipes[index]]); // adds the recipe selected at this index
+      nextItem();
+    }
+    return (
+      
+        <>
+        <ScrollView contentContainerStyle={[styles.container, { flexGrow: 1 }]}>
+
+        <Text style={styles.header}>Find tailored recipes!</Text>
+
+        {/* <ImageViewer imgSource={PlaceholderImage} selectedImage={selectedImage} /> */}
+        {/* <CustomButton theme="primary" label="Upload Receipt" onPress={pickImageAsync} /> */}
+        
+        <View style={styles.inputContainer}>
+            <TouchableOpacity onPress={handleAddItem}>
+            <Ionicons name="add" size={20} color="gray" style={styles.inputIcon} />
+            </TouchableOpacity>
+            <TextInput
+            style={styles.inputBox}
+            value={inputText}
+            onChangeText={(inputText) => setInputText(inputText)}
+            onSubmitEditing={handleAddItem}
+            placeholder="Enter ingredient"
+            placeholderTextColor="#999"
+            returnKeyType="done"
+            />
+        </View>
+
+        <View style={styles.buttonContainer}>
+            <TouchableOpacity
+                style={[styles.button, styles.selectedButton]}
+                onPress={() => setShoppingList([])} // Clear List
+            >
+                <Text style={styles.buttonText}>Clear Ingredients</Text>
+            </TouchableOpacity>
+        </View>
+                <TouchableOpacity
+            style={styles.selectedButton}
+            onPress={toggleOverlay}>
+              <Text style={styles.buttonText}>Build List!</Text>
+            
+        </TouchableOpacity>
+        
+        <FlatList
+            data={shoppingList}
+            keyExtractor={(item, index) => index.toString()}
+            renderItem={({ item, index }) => (
+            <View style={styles.ingredientItem}>
+                <TouchableOpacity onPress={() => removeIngredient(index)} style={styles.removeButton}>
+                <Ionicons name="remove" size={20} color="gray" />
+                </TouchableOpacity>
+                <TextInput
+                value={item}
+                onChangeText={(item) => updateIngredient(item, index)}
+                />
+            </View>
+            )}
+        />
+
+
+        </ScrollView>
+        {showOverlay && (
+          
+        <View style={styles.overlay}>
+                <View style={styles.overlayHeader}>
+                  
+
+                </View>
+                <ScrollView contentContainerStyle={{ paddingBottom: 550 }}>
+                  
+                  {index < recipes.length ? (
+                    <Item
+                    title={recipes[index].recipeName}
+                    img={recipes[index].img}
+                    keywords={recipes[index].keywords.map(k => k.charAt(0).toUpperCase() + k.slice(1))}
+                    onPress={() => {}}
+                    />
+                  ): <Text>No more recipes to show!</Text>}
+
+
+                  
+
+
+                  <View style={styles.confirmationBtnContainer}>
+                  <TouchableOpacity style={styles.greenYesButton} onPress={() => addRecipe()}><Ionicons name="checkmark" size={50}/></TouchableOpacity>
+                  <TouchableOpacity style={styles.redNoButton} onPress={ ()=> nextItem()}><Ionicons name="close" size={50}/></TouchableOpacity>  {/* move onto next recipe */}
+                  </View>
+                </ScrollView>
+                
+              </View>
+              )}
+
+        <View style={styles.footerHeader}>
+        <TouchableOpacity onPress={() => router.push("/")} style={styles.footerIconLeft}>
+            <Ionicons name="arrow-back" size={20} color="#333333" />
+        </TouchableOpacity>
+
+        <View style={{ flex: 1 }} />
+        </View>
+    </>
+    );
+    }
+
+const styles = StyleSheet.create({
+  overlay: {
+    position: 'absolute',
+    flex:1,
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    backgroundColor: 'rgba(0,0,0,0.85)',
+    padding: 0,
+    zIndex: 1000,
+  },
+  overlayHeader: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginTop: 10,
+    marginBottom: 20,
+  },
+  overlayTitle: {
+    fontSize: 26,
+    fontWeight: 'bold',
+    color: '#FAFAFC',
+  }, 
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: "#FAFAFC",
+    justifyContent: 'space-between',
+  },
+  header: {
+    fontSize: 26,
+    fontWeight: 'bold',
+    marginTop: 10,
+    marginBottom: 20,
+    color: "#333333",
+    fontFamily: "System",
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: "#FAFAFC",
+    borderRadius: 12,
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+    elevation: 2,
+    marginBottom: 20,
+  },
+  inputIcon: {
+    marginLeft: 3,
+    marginRight: 3,
+  },
+  inputBox: {
+    flex: 1,
+    height: 40,
+    fontFamily: "System",
+    color: "#333333",
+    borderWidth: 1,
+    borderColor: "#FAFAFC",
+    borderRadius: 8,
+    paddingHorizontal: 10,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    width: '100%',
+    justifyContent: 'space-between',
+    marginTop: 0,
+  },
+  button: {
+      backgroundColor: '#FAFAFC',
+      paddingHorizontal: 16,
+      paddingVertical: 12,
+      borderRadius: 20,
+      marginBottom: 20,
+      shadowColor: '#000',
+      shadowOffset: { width: 0, height: 2 },
+      shadowOpacity: 0.1,
+      shadowRadius: 4,
+      elevation: 2,
+      alignItems: 'center',
+      justifyContent: 'center',
+      width: '48%',
+  },
+  selectedButton: {
+      backgroundColor: '#FA6163',
+  },
+  buttonText: {
+      fontSize: 14,
+      color: 'white',
+      fontWeight: 'bold',
+      fontFamily: 'System',
+  },
+  findRecipesButtonContainer: {
+    marginTop: 20,
+    alignItems: 'center',
+  },
+  findRecipesButton: {
+    backgroundColor: '#FA6163',
+    paddingHorizontal: 16,
+    paddingVertical: 12,
+    borderRadius: 20,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+    elevation: 2,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+  },
+  findRecipesButtonText: {
+    fontSize: 16,
+    color: 'white',
+    fontWeight: 'bold',
+    fontFamily: 'System',
+  },
+  title: {
+    fontSize: 20,
+  },
+  ingredientItem: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: "#F0F0F0", // Light gray background
+    borderRadius: 12,
+    marginBottom: 10,
+    paddingHorizontal: 10,
+    paddingVertical: 10,
+  },
+  removeButton: {
+    paddingRight: 10,
+  },
+
+    // STYLES FOR RECIPE TILES
+
+    item: {
+      height: 150,
+      marginBottom: 20,
+      borderRadius: 12,
+      overflow: "hidden",
+      shadowColor: "#000",
+      shadowOffset: { width: 0, height: 2 },
+      shadowOpacity: 0.1,
+      shadowRadius: 4,
+      elevation: 2,
+      backgroundColor: "#FAFAFC",
+    },
+    foodImage: {
+        flex: 1,
+        justifyContent: 'flex-end',
+    },
+    textContainer: {
+        padding: 16,
+        backgroundColor: 'rgba(0,0,0,0.4)',
+    },
+    recipeTitle: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        color: '#FAFAFC',
+        marginBottom: 8,
+    },
+    labelsContainer: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+    },
+    label: {
+        backgroundColor: "#FA6163",
+        paddingHorizontal: 12,
+        paddingVertical: 6,
+        borderRadius: 20,
+        marginRight: 8,
+        marginTop: 4,
+    },
+    labelText: {
+        color: "white",
+        fontSize: 10,
+    },
+    confirmationBtnContainer:{
+      position:'absolute',
+      bottom:100,
+      flexDirection:'row',
+      justifyContent:'space-between',
+      width:'80%',
+      alignSelf:'center'
+    },
+    greenYesButton:{
+      borderRadius:50,
+      backgroundColor:"green",
+      width:100,
+      height:100,
+      justifyContent: 'center',
+      alignItems: 'center',
+
+    },
+    redNoButton:{
+      borderRadius:50,
+      backgroundColor:"red",
+      width:100,
+      height:100,
+      justifyContent: 'center',
+      alignItems: 'center',
+
+    },
+    // FOOTER
+
+    footerHeader: {
+      flexDirection: "row",
+      alignItems: "center",
+      justifyContent: "space-between",
+      backgroundColor: "#FAFAFC",
+      paddingHorizontal: 20,
+      paddingVertical: 20,
+      borderTopWidth: 1,
+      borderTopColor: "#eee",
+      elevation: 4, // Android shadow
+      shadowColor: "#000", // iOS shadow
+      shadowOffset: { width: 0, height: -2 },
+      shadowOpacity: 0.1,
+      shadowRadius: 4,
+    },
+    footerIconLeft: {
+      marginLeft: 20,
+    },
+    footerIconRight: {
+      marginRight: 20,
+    },
+});
+
+
